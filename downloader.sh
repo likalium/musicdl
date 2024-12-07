ddf818a4096dc437df558558b70a9b6eb377943d
likalium <marcedeline@pm.me>
2024-12-07 19:47:33 +0100

    Added Streamrip download functions, did minor fixes, updated README

--- downloader.sh
+++ downloader.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-##########################################################################
-#### Little script to symplify music download with Streamrip and OrpheusDL
-##########################################################################
+###################################################################
+#### Script to symplify music download with Streamrip and OrpheusDL
+###################################################################
 
 # Variables to use ANSI color codes while keeping the code readable
 GREEN="\e[32m"
@@ -11,7 +11,7 @@ YELLOW="\e[33m"
 RESET="\e[0m"
 
 # Variables private to the script
-# Possible values for modules (they're here because we print this array on OrpheusDL help
+# Possible values for OrpheusDL modules (they're here because we print this array on OrpheusDL help
 possibleModules=("beatport" "bugs" "deezer" "idagio" "jiosaavn" "kkbox" "napster" "nugs" "qobuz" "soundcloud" "tidal")
 toDownload=() # Variable that will store the list of things to download
 # Dedicated to store the state of the download directory before any download happen, so we know which files the download added
@@ -22,7 +22,7 @@ backupDirState=()
 PLATFORM="qobuz" # Streaming platform to download music from
 TYPE="album" # Media type. Only used for qobuz
 ORPHEUSDIR="$PWD/OrpheusDL" # directory where orpheusdl script is located
-STREAMRIPDIR="$PWD/streamrip" # directory where streamrip script is located
+STREAMRIPDIR="$PWD/Streamrip" # directory where streamrip downloads are located (script is in the python venv)
 DEST="" # Destination to put the downloaded files at the end
 ORPHEUSVENV="./.venv-orpheus" # Venv for orpheusdl
 STREAMRIPVENV="./venv-streamrip" # Venv for streamrip
@@ -63,9 +63,16 @@ checkDirectory () {
 # Argument is the type of media the user gave
 assignType () {
 	userType=$(echo "$1" | tr "\[A-Z\]" "\[a-z\]") # We make the type provided by the user lowercase, just in case
-	# Type must be album, artist, label, track
+	# Treating the case where the platform is qobuz
+	if [[ "$PLATFORM" == "qobuz" ]]; then
+		possibleTypes="album artist label track"
+	# Treating the case where the platform is deezer
+	elif [[ "$PLATFORM" == "deezer" ]]; then
+		possibleTypes="artist track playlist album episode"
+	fi
+	# Checking if type provided by the user is valid, depending on the platform he chose
 	# For this we create a string that contains the possible arguments, separated by a space, and we compare it to a regex containing the type given by the user
-	if [[ "album artist label track" =~ (^|[[:space:]])$userType($|[[:space:]]) ]]; then
+	if [[ "$possibleTypes" =~ (^|[[:space:]])$userType($|[[:space:]]) ]]; then
 		TYPE=$userType
 	# If type is wrong, we exit with an error 
 	else
@@ -156,17 +163,22 @@ addElements () {
 # Function to convert elements into links (for example if they're qobuz ids)
 convertElements () {
 	newToDownload=() # We create an array that will store the new values
+	# We choose the prefix we'll want to add before each element
 	if [[ "$PLATFORM" == "qobuz" ]]; then
-		for i in "${toDownload[@]}"; do
-			# If element doesn't start with "https" then it's not a link, so it's an id
-			if [[ "${i:0:5}" != "https" ]]; then
-				newToDownload+=("https://play.qobuz.com/${TYPE}/${i}")
-			else
-				newToDownload+=("${i}")
-			fi
-		done
-		toDownload=("${newToDownload[@]}") # We replace toDownload with the new values
+		prefix="https://play.qobuz.com/${TYPE}"
+	elif [[ "$PLATFORM" == "deezer" ]]; then
+		prefix="https://www.deezer.com/${TYPE}"
 	fi
+	# Analyzing each element of toDownload
+	for i in "${toDownload[@]}"; do
+		# If element doesn't start with "https" then it's not a link, so it's an id
+		if [[ "${i:0:5}" != "https" ]]; then
+			newToDownload+=("${prefix}/${i}")
+		else
+			newToDownload+=("${i}")
+		fi
+	done
+	toDownload=("${newToDownload[@]}") # We replace toDownload with the new values
 }
 
 # Useful links for music piracy
@@ -194,7 +206,7 @@ ${RED}Usage:${RESET} $0 [OPTIONS] COMMAND
     ${GREEN}-p, --platform${RESET}	Platform you want to download from
     ${GREEN}-m, --move${RESET}		Where to move the files after download. If unset, files won't be moved.
     ${GREEN}-v, --venv${RESET}		Path to the virtual environment
-    ${GREEN}-t, --type${RESET}		Type of the media you want to download. ${RED}Required only if you want to download from Qobuz.${RESET}
+    ${GREEN}-t, --type${RESET}		Type of the media you want to download. ${RED}Required only if you provide end of URLs rather than full URLs${RESET}
     ${GREEN}-z, --zfill${RESET}		Add a zero before each downloaded track number if needed
   
   ${RED}Commands:${RESET}
@@ -218,12 +230,12 @@ ${RED}Usage:${RESET} $0 orpheus [OPTIONS]
     ${GREEN}--module${RESET}		Install the wanted OrpheusDL module (supported: ${YELLOW}${possibleModules[*]}${RESET})
     ${GREEN}-h, --help${RESET}		Show this message and exit
     ${GREEN}-d, --download${RESET}	${YELLOW}Content${RESET} to download, each element being separated by a space. If ${YELLOW}unset${RESET}, switch to interactive download mode.
-    ${GREEN}-i, --install${RESET}	Install OrpheusDL & creates a python virtualenv at $VENV
+    ${GREEN}-i, --install${RESET}	Install OrpheusDL & creates a python virtualenv at ${YELLOW}$VENV${RESET}
 
 
   ${RED}Definitions:${RESET}
     ${YELLOW}content${RESET}	Something to download. Can be an URL or, in case of qobuz, an album id, an artist id...
-    ${YELLOW}unset${RESET}	To let the --download option unset, you can not put it in your command, or put it without any argument, and add options after it
+    ${YELLOW}unset${RESET}	To let the --download option unset, you can not put it in your command, or put it without giving any value
 "
 }
 
@@ -245,9 +257,7 @@ downloadOrpheus () {
 	source "$VENV/bin/activate" # Source the virtual environment
 	# Entering into OrpheusDL directory, exit with an error in case something wrong happens (you cant be too careful i guess)
 	cd "$ORPHEUSDIR" || (echo -e "${RED}ERROR:${RESET} Can't cd into the directory were OrpheusDL is supposed to be"; exit 1)
-	for i in "${toDownload[@]}"; do
-		python "orpheus.py" "$i"
-	done
+	python "orpheus.py" "${toDownload[@]}"
 	# going back to where the command has been launch; go to home if it fails; exit with an error if even this fails
 	cd "$previousDir" ||
 		(echo -e "${YELLOW}Warning${RESET}: Can't find the directory you launched the command from, cd'ing in $HOME"; cd "$HOME") ||
@@ -256,9 +266,9 @@ downloadOrpheus () {
 
 # Function to install OrpheusDL
 installOrpheus () {
-	echo -e "${GREEN}Cloning OrpheusDL...${RESET} \n"
+	echo -e "${GREEN}Cloning OrpheusDL into directory ${YELLOW}${ORPHEUSDIR}${GREEN}...${RESET} \n"
 	git clone "https://github.com/OrfiTeam/OrpheusDL" "$ORPHEUSDIR" || echoError "Can't clone OrpheusDL github repository. Maybe check your internet connection?"
-	echo -e "${GREEN}Creating the python virtual environment for OrpheusDL at ${YELLOW}${VENV}${YELLOW}...${RESET} \n"
+	echo -e "${GREEN}Creating the python virtual environment for OrpheusDL at ${YELLOW}${VENV}${GREEN}...${RESET} \n"
 	python -m venv "$VENV" || echoError "Can't create the python virtual environment. Check python is correctly installed."
 	echo -e "${GREEN}Installing OrpheusDL requirements...${RESET}"
 	# Sourcing the virtual environment, exit with an error if something wrong happens
@@ -338,26 +348,26 @@ orpheusModules () {
 
 # Main command (like a hub) for OrpheusDL related argument parsing
 orpheus () {
+	# If VENV is unset, call it .venv-orpheus
+	if [[ "$VENV" == "" ]]; then
+		VENV="./.venv-orpheus"
+	fi
 	# If no arguments at all, print help and exit
 	if [[ $# == 0 ]]; then
 		orpheusHelp
 		exit 0
 	fi
-	# Choose weather to print help or not regarding of the arguments
+	# Choose whether to print help or not regarding of the arguments
 	for i in "$@"; do
 		# Arguments starts by "-" so we check only elements that starts by "-"
 		if [[ "${i:0:1}" == "-" ]]; then
 			# If any argument is "-h" or "--help", or if there is an unrecognized argument, print help and exit without doing anything
-			if [[ "--help -h" =~ (^|[[:space:]])$i($|[[:space:]]) || ! "-i --install -d --download --module" =~ (^|[[:space:]])$i($|[[:space:]]) ]]; then
+			if [[ "--help -h" =~ (^|[[:space:]])$i($|[[:space:]]) || ! "-i --install -d --download -m --module" =~ (^|[[:space:]])$i($|[[:space:]]) ]]; then
 				orpheusHelp
 				exit 0
 			fi
 		fi
 	done
-	# If VENV is unset, call it .venv-orpheus
-	if [[ "$VENV" == "" ]]; then
-		VENV="./.venv-orpheus"
-	fi
 	# If no help asked, we parse the arguments one by one. We parse until there is no argument left
 	while [[ $# -gt 0 ]]; do
 		case "$1" in
@@ -368,7 +378,7 @@ orpheus () {
 			"-i" | "--install" )
 				installOrpheus
 				;;
-			"--module")
+			"-m" | "--module")
 				orpheusModules "${@:2}" # We pass all after the -m/--module as arguments
 				;;
 		esac
@@ -392,48 +402,85 @@ ${RED}Usage:${RESET} $0 streamrip [OPTIONS]
 
   ${RED}Options:${RESET}
     ${GREEN}-h, --help${RESET}		Show this message and exit
-    ${GREEN}-v, --venv${RESET}		Path to the virtual environment
-    ${GREEN}-p, --platform${RESET}	Platform you want to download from
-    ${GREEN}-t, --type${RESET}		Type of the media you want to download
-    ${GREEN}-i, --install${RESET}	Install Streamrip & creates a python virtualenv at $PWD/.streamrip
-    ${GREEN}-d,--download${RESET}	Links to the content to download (separated by spaces)
-    ${GREEN}-m, --move${RESET}		Where to move the files after download. If unset, files won't be moved
+    ${GREEN}-d, --download${RESET}	${YELLOW}Content${RESET} to download, each element being separated by a space. If ${YELLOW}unset${RESET}, switch to interactive download mode.
+    ${GREEN}-i, --install${RESET}	Creates a python virtual environment at ${YELLOW}${VENV}${RESET} and install Streamrip into it
+
+  ${RED}Definitions:${RESET}
+    ${YELLOW}content${RESET}	Something to download. Can be an URL or, in case of qobuz, an album id, an artist id...
+    ${YELLOW}unset${RESET}	To let the --download option unset, you can not put it in your command, or put it without giving any value
 "
 }
 
 # Function to install Streamrip
 installStreamrip () {
-	echo -e "${GREEN}Creating the python virtual environment for Streamrip...${RESET} \n"
-	python -m venv .venv-streamrip
-	echo -e "${GREEN}Installing Streamrip...${RESET} \n"
-	source ./.venv-streamrip/bin/activate # Sourcing the virtual environment
-	pip install --upgrade streamrip
+	echo -e "${GREEN}Creating the python virtual environment for Streamrip at ${YELLOW}${VENV}${GREEN}...${RESET} \n"
+	python -m venv "$VENV" || echoError "Can't create the python virtual environment. Check python is correctly installed."
+	echo -e "${GREEN}Installing Streamrip into the newly created virtual environment...${RESET}"
+	# Sourcing the virtual environment, exit with an error if something wrong happens
+	source "$VENV/bin/activate" || echoError "Can't activate the virtual environment. Check your python installation."
+	# Installing streamrip with pip
+	pip3 install streamrip --upgrade || echoError "Can't install Streamrip. Check your python installation."
 	deactivate # Deactivating the virtual environment
-	echo -e "${GREEN}\nDone!${RESET}"
+	echo -e "${GREEN}Create directory where Streamrip downloads will be put into (${YELLOW}${STREAMRIPDIR}${GREEN})...${RESET}"
+	mkdir -p "${STREAMRIPDIR}" || echoError "Can't create streamrip downloads directory"
+	echo -e "\n ${GREEN}Done!${RESET}"
+}
+
+# Function to download items with streamrip
+downloadStreamrip () {
+	previousDir="$PWD" # We save the directory the program is executed from
+	checkDirectory "$STREAMRIPDIR" "folder to download items into" # We check if the destination given to download files exists
+	source "$VENV/bin/activate" || echoError "Can't activate virtual environment, check your python installation" # Source the virtual environment
+	# Entering into OrpheusDL directory, exit with an error in case something wrong happens (you cant be too careful i guess)
+	rip url "${toDownload[@]}" || echoError "Can't download songs with streamrip, check your python installation and your streamrip installation."
+	deactivate # Deactivate the python venv
+	# going back to where the command has been launch; go to home if it fails; exit with an error if even this fails
+	cd "$previousDir" ||
+		(echo -e "${YELLOW}Warning${RESET}: Can't find the directory you launched the command from, cd'ing in $HOME"; cd "$HOME") ||
+		echoError "Can't find nor the directory you were in, nor an home directory"
 }
 
 # Main command (like a hub) for Streamrip related argument parsing
 streamrip () {
-	# If no arguments at all, we print help
-	if  [[ $# == 0 ]]; then
-		streamripHelp
-		exit 0 # We instantly exit after print help message
+	# If venv is unset, we call it ./.venv-streamrip
+	if [[ "$VENV" == "" ]]; then
+		VENV="./.venv-streamrip"
 	fi
+	# If no arguments at all, print help and exit
+	if [[ $# == 0 ]]; then
+		streamripHelp
+		exit 0
+	fi
+	# Choose whether to print help or not regarding of the arguments
+	for i in "$@"; do
+		# Arguments starts by "-" so we check only elements that starts by "-"
+		if [[ "${i:0:1}" == "-" ]]; then
+			# If any argument is "-h" or "--help", or if there is an unrecognized argument, print help and exit without doing anything
+			if [[ "--help -h" =~ (^|[[:space:]])$i($|[[:space:]]) || ! "-i --install -d --download" =~ (^|[[:space:]])$i($|[[:space:]]) ]]; then
+				streamripHelp
+				exit 0
+			fi
+		fi
+	done
 	# Parsing the arguments while there is arguments
 	while [[ $# -gt 0 ]]; do
-		print $#
 		# We treat the first argument
 		case $1 in
+			"-d" | "--download")
+				addElements "${@:2}" # Adding elements to download
+				convertElements # Convert elements if needed
+				;;
 			"-i" | "--install")
-				installStreamrip ;;
-			# We print help if the users uses help flag, or if argument isn't recognized
-			"-h" | "--help" | *)
-				streamripHelp
-				exit 0 ;; # We instantly exit after print help message
+				installStreamrip
+				;;
 		esac
 		# We shift the positional parameters to the left (2nd argument replaces 1st one, 3rd argument replaces 2nd...)
 		shift
 	done
+	# We launched download after all argument have been parsed and if toDownload isn't empty
+	if [[ ${#toDownload[@]} -gt 0 ]]; then
+		downloadStreamrip
+	fi
 }
 
 ################
